256dff1fd77b44e37c9c46096624a0cb112222b9
Divyam Divesh <divyamdivesh13@gmail.com>
2025-06-01 23:00:27 +0530

    connected with mongodb and shortid generated

---
+++ controllers/url.js
@@ -0,0 +1,21 @@
+const shortid = require('shortid');
+const URL = require('../models/url');
+
+async function handleGenerateNewShortUrl(req, res){
+    const body = req.body;
+    if (!body || !body.url) {
+        return res.status(400).json({error: 'URL is required'});
+    }
+    const shortId = shortid();
+    await URL.create({
+        shortId: shortId,
+        redirectUrl: body.url,
+        visitHistory: []
+    });
+
+    return res.json({id:shortId})
+}
+
+module.exports = {
+    handleGenerateNewShortUrl,
+}
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
+const { connectToMongoDB } = require("./connect");
+const urlRoutes = require("./routes/url");
 
 const app = express();
 const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url")
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+app.use("/url", urlRoutes);
 
 //Starting Server
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
